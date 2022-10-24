ba476501bfb755cb9904dc066c2708cbe26f586d
gideonekekeke <gideonekeke64@gmail.com>
2022-10-24 12:56:43 +0100

    environment variable for mongodb

--- index.ts
+++ index.ts
@@ -1,16 +1,19 @@
+import "dotenv/config";
 import express, { Response, Request, Application } from "express";
 const port: number | string = process.env.PORT || 5000;
 import mongoose from "mongoose";
 const URL: string = "mongodb://localhost/AJwalletDB";
+
 import user from "./Route/UserRouter";
 import fol from "./Route/FollowRoute";
 import wallet from "./Route/WalletRoute";
 import history from "./Route/HistoryRoute";
+import cors from "cors";
 
 const app: Application = express();
 
 mongoose
-	.connect(URL)
+	.connect(`${process.env.MONGODB_URI}`)
 	.then(() => {
 		console.log("database is connected");
 	})
@@ -23,10 +26,8 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use(express.json());
-
-app.get("/", (req: Request, res: Response) => {
-	res.status(200).json({ message: "api is ready for consumption" });
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/api/users", user);
 app.use("/api/follow", fol);
